a26e59941919a047ba8ed2c5c451a87c0e009c3e
Elan456 <telan4892@gmail.com>
2024-11-27 22:00:21 -0500

    n=2 100% misleading accuracy with gemini

--- misleading_image/gemini.py
+++ misleading_image/gemini.py
@@ -17,15 +17,44 @@ class Gemini:
 gemini = Gemini()
 
 def gemini_filter_misleading_images(tweets: List[TweetWithContext]):
-    prompt_base = json.load("misleading_image/prompts/p1.json")
+    # Pre load and rewrite the json file without any non-ascii characters
+    # pj = json.load(open("misleading_image/prompts/p1.json", "r", encoding="utf-8"))
+    # with open("misleading_image/prompts/p1.json", "w") as f:
+    #     json.dump(pj, f, ensure_ascii=True, indent=4)
+
+
+    prompt_base_json = json.load(open("misleading_image/prompts/p1.json", "r"))
+
+    prompt_base = ["A contextual image is an image that isn't intended to mislead, but can be taken out of context if the tweet text doesn't provide enough information. A misleading image is an image that is intended to mislead the viewer on it's own regardless of the tweet text. Please determine if the image is contextual or misleading based on the tweet text and image."]
+    prompt_base.append("Below are a few examples")
+    for item in prompt_base_json["entries"]:
+        for key, value in item.items():
+            if key != "img_path":
+                if key == "classification":
+                    key = "Image Classification"
+                prompt_base.append(f"{key}: {value}")
+            else:
+                img_path = value
+                img = Image.open(img_path)
+                prompt_base.append(img)
 
     for tweet in tweets:
         prompt = prompt_base.copy()
-        prompt["input"]["text"] = tweet.text
-        prompt["input"]["image"] = tweet.image_path
-        prompt["input"]["community_note"] = tweet.community_note
-
+        prompt.append("Is the image itself a contextual image or misleading image? If the image itself isn't misleading but is being used incorrectly, then it is a contextual image. If the image is misleading on its own, then it is a misleading image.")
+        prompt.append(f"Tweet: {tweet.text}")
+        prompt.append(f"Community Note: {tweet.community_note}")
+        img = Image.open(tweet.image_path)
+        prompt.append(img)
         response = gemini.generate(prompt)
-        tweet.image_path = response["output"]["image"]
-        tweet.text = response["output"]["text"]
-        tweet.community_note = response["output"]["community_note"]
+
+        print(response)
+
+if __name__ == "__main__": # Quick test
+    tweet_misleading = TweetWithContext("Sex offending rate of women: 3 per one million Sex offending rate of men: 395 per million Sex offending rates of transwomen: 1,916 per million",
+                            "misleading_image/imgs/sex_offending.jpg", 
+                            "The UK government estimates there are between 250,000 and 500,000 trans individuals in the UK.    https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/721642/GEO-LGBT-factsheet.pdf#:~:text=We%20tentatively%20estimate%20that%20there%20are%20approximately%20200%2C000-500%2C000,whether%20and%20how%20to%20develop%20a%20population%20estimate.    This image overstates the rate of such offenses by severely undercounting the trans population. The actual rate using that figure is 5-10x lower, below cisgender men.")
+    
+    tweet_contextual = TweetWithContext("Jaffa Tel Aviv is not safe.","misleading_image/imgs/moscow_fire.jpg",
+                                        "The image is from a gas explosion which happened in Moscow in April 2009.  https://ria.ru/20090510/170619872.html")
+    
+    gemini_filter_misleading_images([tweet_misleading, tweet_contextual])
